6764ea8e00e774cb35b0655cface9be0ae206fac
Brendon Dulam <brendondulam06@gmail.com>
2020-05-09 20:42:47 +0900

    undo

--- main.go
+++ main.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"log"
-	"strconv"
 )
 
-var mem = map[int64]int64{0: 1, 1: 1, 2: 2}
-
-func fibonacci(n int64) int64 {
-	if val, ok := mem[n]; ok {
-		return val
-	}
-
-	return fibonacci(n-1) + fibonacci(n)
-}
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	val := request.QueryStringParameters["val"]
-	valInt, err := strconv.Atoi(val)
-	if err != nil {
-		log.Fatal("Error converting into int", err)
-	}
-
-	fib := fibonacci(int64(valInt))
-
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       fmt.Sprintf("Fib(%d)=%d", valInt, fib),
+		Body:       "Hello Netlify, AWS Lambda",
 	}, nil
 }
 
