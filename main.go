70eba5a90b29a061da0e9644da18577c94ab83b2
Brendon Dulam <brendondulam06@gmail.com>
2020-05-09 21:05:15 +0900

    test sum

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	sum := 0
+	for i := 0; i <= 10000; i++ {
+		sum += i
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello Netlify, AWS Lambda",
+		Body:       fmt.Sprintf("Hello %d", sum),
 	}, nil
 }
 
