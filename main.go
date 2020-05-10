e73604d96f963efa6d830a483e3b4a2ac98d4a22
Brendon Dulam <brendondulam06@gmail.com>
2020-05-10 17:36:52 +0900

    graphql

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/bldulam1/aws-lambda-go-example/src/fib"
+	"github.com/graphql-go/graphql"
+	"log"
+	"time"
 )
 
-func Sum(x int, y int) int {
-	return x + y
-}
-
 func main() {
-	fmt.Println(fib.Factorial(4))
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.DateTime,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return time.Now(), nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	// Query
+	query := `
+		{
+			hello
+		}
+	`
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON) // {“data”:{“hello”:”world”}}
 }
